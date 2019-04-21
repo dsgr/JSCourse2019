97f428b0b19a111af4f51f5f677419f84cb7d083
Sergei.Malkevich <sergei.malkevich@intexsoft.by>
2019-04-21 19:27:32 +0300

    hw_08 task #1

---
+++ hw_08/index.js
@@ -0,0 +1,98 @@
+/**
+ * 1. Управление временем
+ * Необходимо реализовать дополнительные функции для управления датой и
+ * временем.
+ * var date = require(&#39;./index.js&#39;);
+ * var time = date(&#39;2017-05-16 13:45&#39;)
+ * .add(24, &#39;hours&#39;)
+ * .subtract(1, &#39;months&#39;)
+ * .add(3, &#39;days&#39;)
+ * .add(15, &#39;minutes&#39;);
+ * console.info(time.value);
+ * // &quot;2017-04-20 14:00&quot;
+ * В функцию всегда передается строка в правильном и полном формате.
+ * Дополнительных проверок не требуется.
+ * Формат даты — &quot;YYYY‒MM‒DD HH:SS&quot;, где YYYY — год, MM — месяц, DD
+ * — день, HH — час, SS — минуты
+ * В функции add/subtract всегда передается целое число
+ * Гарантируется, что после всех манипуляций получится корректная дата.
+ * Функции add/subtract
+ * Каждая функция принимает первым аргументом количество единиц, на которое
+ * нужно изменить дату, а вторым — единицу измерения.
+ * Можно менять следующие значения: years (годы), months (месяцы), days (дни),
+ * hours (часы), minutes (минуты)
+ * В реализации советую использовать объект Date. В нём реализован правильный
+ * переход через месяц, год, час.
+ * Для решения задачи можно из функции date вернуть объект с методами. Чтобы
+ * эти методы можно было вызывать цепочкой, из каждого метода нужно
+ * возвращать сам объект.
+ */
+function timeShift(inDate) {
+    return {
+        date: new Date(inDate),
+
+        add: function (num, unit) {
+            switch (unit) {
+                case 'minutes':
+                    this.date.setMinutes(this.date.getMinutes() + num);
+                    return this;
+                case 'hours':
+                    this.date.setHours(this.date.getHours() + num);
+                    return this;
+                case 'days':
+                this.date.setDate(this.date.getDate() + num);
+                    return this;
+                case 'months':
+                this.date.setMonths(this.date.getMonths() + num);
+                    return this;
+                case 'years':
+                this.date.setYears(this.date.getYears() + num);
+                    return this;
+            }
+        },
+
+        subtrack: function (num, unit) {
+            switch (unit) {
+                case 'minutes':
+                this.date.setMinutes(this.date.getMinutes() - num);
+                    return this;
+                case 'hours':
+                this.date.setHours(this.date.getHours() - num);
+                    return this;
+                case 'days':
+                this.date.setDate(this.date.getDate() - num);
+                    return this;
+                case 'months':
+                this.date.setMonths(this.date.getMonths() - num);
+                    return this;
+                case 'years':
+                this.date.setYears(this.date.getYears() - num);
+                    return this;
+            }
+        }
+    }
+}
+
+console.log('Task #1');
+console.log(
+    timeShift('2019-04-21 19:55').add(1, 'hours')
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ hw_08/solution.js
@@ -0,0 +1,91 @@
+//Подсказка: сюда можно складывать записи адресной книги.
+var phoneBook = [];
+
+//Здесь можно объявить переменные и функции, которые понядобятся вам для работы ваших функций
+
+module.exports = {
+    getWords: function (input) {
+        return input.split(/^#|[^#]#/g)
+            .slice(1)
+            .map(
+                function (item, i, arr) {
+                    return item.split(" ")[0]
+                }
+            )
+    },
+    normalizeWords: function (inWords) {
+        return inWords
+            .map(function (item, i, arr) {
+                return item.toLowerCase();
+            })
+            .filter(function (item, i, arr) {
+                return arr.indexOf(item) === i;;
+            })
+            .join(", ");
+
+    },
+    addressBook: function (command) {
+        var comandAction = command.split(" ")[0];
+        switch (comandAction) {
+            case "ADD":
+                var comandName = command.split(" ")[1];
+                var comandPhones = command.split(" ")[2];
+                if (phoneBook.some(function (item, pos, arr) {
+                    return item.name === comandName;
+                })) { //если имя уже записано
+                    var arrPos;
+                    phoneBook.some(function (item, pos, arr) {
+                        if (item.name === comandName) {
+                            arrPos = pos;
+                            return true;
+                        }
+                    })
+                    phoneBook[arrPos].phones = phoneBook[arrPos].phones.concat(comandPhones.split(","));
+                } else {
+                    phoneBook.push({
+                        name: comandName,
+                        phones: comandPhones.split(",")
+                    });
+                }
+                break;
+
+            case "SHOW":
+                phoneBook.sort(function (a, b) {
+                    if (a.name < b.name)
+                        return -1;
+                    if (a.name > b.name)
+                        return 1;
+                    return 0;
+                });
+
+                return phoneBook
+                    .filter(function (item, pos, arr) {
+                        return item.phones.length > 0;
+                    })
+                    .map(function (item, pos, arr) {
+                        return item.name + ": " + item.phones.join(", ");
+
+                    })
+                break;
+
+            case "REMOVE_PHONE":
+                var removePhone = command.split(" ")[1];
+                if (phoneBook.every(function (item, pos, arr) {
+                    item.phones.indexOf(removePhone) === -1;
+                })) {
+                    return false;
+
+                } else {
+                    return phoneBook.some(
+                        function (item, pos, arr) {
+                            if (item.phones.indexOf(removePhone) != -1) {
+                                item.phones.splice(pos, 1);
+                                return true;
+                            }
+                        }
+                    );
+                }
+                break;
+        }
+    }
+}
